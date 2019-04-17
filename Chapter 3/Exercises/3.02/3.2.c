d9f8d5b82a3acdb6bcdce979cf2b56dce293caef
Thomas Blakely <thomblakely@gmail.com>
2019-04-17 21:43:15 +1200

    adding exercise 3.02

---
+++ Chapter 3/Exercises/3.02/3.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+   float x = 3.14f;
+
+   printf("a) %8.1e\n", x);
+   printf("b) %-10.6e\n", x);
+   printf("c) %8.3f\n", x);
+   printf("d) %-6.0f\n", x);
+
+   return 0;
+}
