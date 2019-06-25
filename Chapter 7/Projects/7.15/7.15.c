40796e1ed2b019713881b3ada01b781ad28e7b44
tambow44 <thomblakely@gmail.com>
2019-06-25 19:45:18 +1200

    project 7.15

---
+++ Chapter 7/Projects/7.15/7.15.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int intNum, count;
+   long double factorial = 1.0;
+
+   printf("Enter a positive integer: ");
+    scanf("%d", &intNum);
+
+   short shortNum = (short) intNum;
+   long longNum = (long) intNum;
+   long long longLongNum = (long long) intNum;
+   float floatNum = (float) intNum;
+   double doubleNum = (double) intNum;
+   long double longDoubleNum = (long double) intNum;
+
+   for (count = 1; count <= shortNum; count++) {
+      factorial *= count;
+   }
+      printf("a) Factorial of %d: %.0Lf\n", shortNum, factorial);
+
+   for (count = 1; count <= intNum; count++) {
+      factorial *= count;
+   }
+      printf("b) Factorial of %d: %.0Lf\n", intNum, factorial);
+
+   for (count = 1; count <= longNum; count++) {
+      factorial *= count;
+   }
+      printf("c) Factorial of %ld: %.0Lf\n", longNum, factorial);
+
+   for (count = 1; count <= longLongNum; count++) {
+      factorial *= count;
+   }
+      printf("d) Factorial of %lld: %.0Lf\n", longLongNum, factorial);
+
+   for (count = 1; count <= floatNum; count++) {
+      factorial *= count;
+   }
+      printf("e) Factorial of %f: %.0Lf\n", floatNum, factorial);
+
+   for (count = 1; count <= doubleNum; count++) {
+      factorial *= count;
+   }
+      printf("f) Factorial of %f: %.0Lf\n", doubleNum, factorial);
+
+   for (count = 1; count <= longDoubleNum; count++) {
+      factorial *= count;
+   }
+      printf("g) Factorial of %Lf: %.0Lf\n", longDoubleNum, factorial);
+
+   return 0;
+}
