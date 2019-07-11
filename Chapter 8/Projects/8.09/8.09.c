b746cb64ef47bb1ecfb159e359be5ee05d4a8cd5
tambow44 <thomblakely@gmail.com>
2019-07-12 01:21:37 +1200

    8.09 progress

--- Chapter 8/Projects/8.09/8.09.c
+++ Chapter 8/Projects/8.09/8.09.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>  /* srand() & rand() functions */
+#include <stdbool.h> /* TRUE(1) & FALSE(0) */
+#include <time.h>    /* provides 'random' number for srand() */
+
+#define N 10
+#define AZ 26
 
 int main(void)
 {
+   int i, j, num, direction;
+   char board[N][N]; // 10 x 10 board
+   char alphabet = 'A'; // while < 'Z' 
+   
+   srand((unsigned) time(NULL)); /* ensures rand() is always unique */
 
+   /* Generates blank board */
+   for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+         board[i][j] = '.';
+      }
+   }
 
+   /* Start the show with 'A' */
+   board[0][0] = alphabet;
+
+   for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+         direction = (rand() % 4);
+         
+         if (direction == 0) {         // UP
+            
+         } else if (direction == 1) {  // DOWN
+            
+         } else if (direction == 2) {  // LEFT
+            
+         } else if (direction == 3) {  // RIGHT
+            
+         }
+      }
+   }
 
    return 0;
 }
+
+/*
+  A . . . . . . . . .
+  B C D . . . . . . .
+  . F E . . . . . . .
+  H G . . . . . . . .
+  I . . . . . . . . .
+  J . . . . . . . Z .
+  K . . R S T U V Y .
+  L M P Q . . . W X .
+  . N O . . . . . . .
+  . . . . . . . . . .
+*/
