fa2a8058db6889a39761c538c7863c96f0ad2456
Thomas Blakely <thomblakely@gmail.com>
2019-08-02 14:46:55 +1200

    Exercise 9.11 complete

--- Chapter 9/Exercises/9.11/9.11.c
+++ Chapter 9/Exercises/9.11/9.11.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 
+#define LEN 5
+
 float compute_GPA(char grades[], int n)
 {
-   
+   int i;
+   float sum = 0.0f;
 
-   return 1;
+   for (i = 0; i < n; i++) {
+      switch(toupper(grades[i]))  {
+         case 'A':
+            sum += 4.0f;
+            break;
+         case 'B':
+            sum += 3.0f;
+            break;
+         case 'C':
+            sum += 2.0f;
+            break;
+         case 'D':
+            sum += 1.0f;
+            break;
+         case 'F':
+            sum += 0.0f;
+            break;
+         default:
+            break;
+      }
+   }
+
+   return sum / n;
 }
 
 int main(void)
 {
+   char grade[LEN] = { 'A', 'A', 'F', 'D', 'C' };
 
+   printf("Grade average: %.2f\n", compute_GPA(grade, LEN));
+
+   return 0;
 }
-
-/*
-
-The `grades` array will contain letter grades (`A`, `B`, `C`, `D`, or `F`, either upper-case or lower-case); `n` is the length of the array. The function should return the average of the grades (assume that `A = 4`, `B = 3`, C = 2`, `D = 1`, and `F = 0`).
-*/
