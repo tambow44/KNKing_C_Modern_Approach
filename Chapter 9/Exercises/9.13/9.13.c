191ff313c3f264e10fe060281a0433aff23d0454
tambow44 <thomblakely@gmail.com>
2019-08-04 23:13:21 +1200

    Exercise 9.13

--- Chapter 9/Exercises/9.13/9.13.c
+++ Chapter 9/Exercises/9.13/9.13.c
@@ -2,12 +2,48 @@
 
 int evaluate_position(char board[8][8])
 {
+   int i, j, black = 0, white = 0;
 
-   return 0;
+   for (i = 0; i < 8; i++) {
+      for (j = 0; j < 8; j++) {
+         /* White pieces */
+         board[i][j] == 'Q' ? white += 9 : white ;
+         board[i][j] == 'R' ? white += 5 : white ;
+         board[i][j] == 'B' ? white += 3 : white ;
+         board[i][j] == 'N' ? white += 3 : white ;
+         board[i][j] == 'P' ? white += 1 : white ;
+         /* Black pieces */
+         board[i][j] == 'q' ? black += 9 : black ;
+         board[i][j] == 'r' ? black += 5 : black ;
+         board[i][j] == 'b' ? black += 3 : black ;
+         board[i][j] == 'n' ? black += 3 : black ;
+         board[i][j] == 'p' ? black += 1 : black ;
+      }
+   }
+
+   return white - black;
 }
 
 int main(void)
 {
+   char board[8][8] = {{ 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' },
+                       { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+                       { 'r', 'n', 'b' ,'q', 'k', 'b', 'n', 'r' }};
+
+   int result = evaluate_position(board);
+
+   if (result == 0) {
+      printf("There is no advantage.\n");
+   } else if (result > 0) {
+      printf("White has the advantage with %d points.\n", result);
+   } else if (result < 0) {
+      printf("Black has the advantage with %d points.\n", result);
+   }
 
    return 0;
 }
--- Chapter 9/Exercises/9.13/README.md
+++ Chapter 9/Exercises/9.13/README.md
@@ -10,3 +10,42 @@ See `9.13.c` for complete program.
 
 Function is as follows:
 ```c
+int evaluate_position(char board[8][8])
+{
+   int i, j, black = 0, white = 0;
+
+   for (i = 0; i < 8; i++) {
+      for (j = 0; j < 8; j++) {
+         /* White pieces */
+         board[i][j] == 'Q' ? white += 9 : white ;
+         board[i][j] == 'R' ? white += 5 : white ;
+         board[i][j] == 'B' ? white += 3 : white ;
+         board[i][j] == 'N' ? white += 3 : white ;
+         board[i][j] == 'P' ? white += 1 : white ;
+         /* Black pieces */
+         board[i][j] == 'q' ? black += 9 : black ;
+         board[i][j] == 'r' ? black += 5 : black ;
+         board[i][j] == 'b' ? black += 3 : black ;
+         board[i][j] == 'n' ? black += 3 : black ;
+         board[i][j] == 'p' ? black += 1 : black ;
+      }
+   }
+
+   return white - black;
+}
+```
+#### Output
+Where:
+```c
+   char board[8][8] = {{ 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' },
+                       { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { '.', '.', '.', '.', '.', '.', '.', '.' },
+                       { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+                       { 'r', 'n', 'b' ,'q', 'k', 'b', 'n', 'r' }};
+```
+```
+There is no advantage.
+```
