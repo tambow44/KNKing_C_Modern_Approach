6078281a515d344d53c167867fe0e682e2c2dc6f
tambow44 <thomblakely@gmail.com>
2019-08-09 05:21:06 +1200

    Project 9.08

--- Chapter 9/Projects/9.08/9.08.c
+++ Chapter 9/Projects/9.08/9.08.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <ctype.h>
 
-int point = 0, win = 0, lose = 0;
+int point = 0;
 
 int roll_dice(void)
 {
@@ -15,66 +15,30 @@ bool play_game(void)
 {
    int dice = roll_dice();
 
-   printf("\nFUNCTION win:%02d\tlose: %02d----\n", win, lose);
-
-//   printf("You rolled: %d\n", dice);
+   printf("You rolled: %d\n", dice);
    if (dice == 7 || dice == 11 || dice == point) {
-      win++;
-      printf("--------win:  %d\n", win);
       return true;
    } else if (dice == 2 || dice == 3 || dice == 12) {
-      lose++;
-      printf("--------lose: %d\n", lose);
       return false;
    } else if (point == 0) {
       point = dice;
-//      printf("Your point is %d\n", point);
+      printf("Your point is %d\n", point);
    }
-   printf("\nFUNCTION win:%02d\tlose: %02d----\n", win, lose);
-   play_game();
 
+   play_game();
+   return 0;
 }
 
 int main(void)
 {
    bool game = true;
-   int /*win = 0, lose = 0,*/ i, w = 0, l = 0;
+   int win = 0, lose = 0;
    char ch;
 
    srand((unsigned) time(NULL));
 
-   printf("GLOBAL: win: %02d\tlose: %02d\n", win, lose);
-   printf("LOCAL : win: %02d\tlose: %02d\n", w, l);
-
-   for (i = 0; i < 5; i++) {
-
-      if (play_game() == true) {
-         w++;
-         printf("returns true!! %d\n\n", w);
-      } else {
-         l++;
-         printf("returns false!! %d\n\n", l);
-      }
-
-   }
-
-   printf("GLOBAL: win: %02d\tlose: %02d\n", win, lose);
-   printf("LOCAL : win: %02d\tlose: %02d\n", w, l);
-
-   return 1;
-/*
-   printf("Let's play a game:\n");
-   for (i = 0; i < 5; i++) {
-      play_game();//? printf("we have won!\n") : printf("we have lost!\n");
-      printf("\n");
-   }
-
-   printf("win: %d, lose: %d\n", win, lose);
-   
-   return 1;
-*/
    while (game) {
-      play_game() ? printf("You win!\n") : printf("You lose!\n") ;
+      (play_game()) ? printf("You win!\n"), win++ : printf("You lose!\n"), lose++ ;
 
       printf("\nPlay again? ");
        scanf(" %c", &ch);
