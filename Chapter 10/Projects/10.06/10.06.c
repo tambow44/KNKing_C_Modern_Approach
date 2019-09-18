d2bf638565ea6ad5c34e079eb0d7825bbaba0e39
Thomas Blakely <thomblakely@gmail.com>
2019-09-19 11:44:20 +1200

    Project 10.06, working

---
+++ Chapter 10/Projects/10.06/10.06.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define STACK_SIZE 10
+
+int main(void)
+{
+   char ch;
+   int i = 0;
+
+   printf("Enter an RPN expression: ");
+   while ((ch = getchar() != [0 1 2 3 4 5 6 7 8 9]))
+      i++;
+
+   printf("i: %d\n", i);
+
+
+   return 0;
+}
