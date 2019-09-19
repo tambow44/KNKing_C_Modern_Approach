cb5a8ad91002cb4679bbf0ef0d5429cbb73a9fe1
Thomas Blakely <thomblakely@gmail.com>
2019-09-19 16:25:07 +1200

    Shambles

--- Chapter 10/Projects/10.06/10.06.c
+++ Chapter 10/Projects/10.06/10.06.c
@@ -1,19 +1,88 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define STACK_SIZE 10
+/* Macro Definitions */
+#define SIZE 101
 
+/* External Variables */
+int stack[SIZE];
+int top = -1;
+
+/* Function Definitions */
+void make_empty();
+void push(int i);
+void pop();
+ int top_stack();
+
+/* Main body */
 int main(void)
 {
-   char ch;
-   int i = 0;
+   char ch, oper1, oper2;
 
-   printf("Enter an RPN expression: ");
-   while ((ch = getchar() != [0 1 2 3 4 5 6 7 8 9]))
-      i++;
+   while (true) {
+      printf("Enter an RPN expression: ");
 
-   printf("i: %d\n", i);
-
+      while ((ch = getchar()) != '\n') {
+         if (ch == 'q' || ch == 'Q') 
+            return 0;
+         else if (ch == '=')
+            printf("Value of expression: %d\n", top_stack());
+         else
+            switch (ch) {
+               case '*' : 
+                  oper1 = top_stack(); pop();
+                  oper2 = top_stack(); pop(); 
+                  push(oper1 * oper2);
+                  break;
+               case '/' : 
+                  oper1 = top_stack(); pop();
+                  oper2 = top_stack(); pop(); 
+                  push(oper1 / oper2);
+                  break;
+               case '+' : 
+                  oper1 = top_stack(); pop();
+                  oper2 = top_stack(); pop(); 
+                  push(oper1 + oper2);
+                  break;
+               case '-' : 
+                  oper1 = top_stack(); pop();
+                  oper2 = top_stack(); pop(); 
+                  push(oper1 - oper2);
+                  break;
+               default  :
+                  push(ch);
+                  break;
+            }
+      }
+   }
 
    return 0;
+}
+
+/* Clear the stack */
+void make_empty()
+{
+   for (int i = 0; i < SIZE; i++)
+      top--;
+}
+
+void push(int i)
+{
+   if (top == SIZE - 1)
+      printf("ERROR: Stack Overflow!\n");
+   else
+      stack[++top] = i;
+}
+
+void pop()
+{
+   if (top == -1)
+      printf("ERROR: Stack underflow!\n");
+   else
+      top--;
+}
+
+int top_stack()
+{
+   return stack[top];
 }
